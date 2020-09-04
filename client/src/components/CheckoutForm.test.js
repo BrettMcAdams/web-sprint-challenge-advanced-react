742ece9a7b464840e216d2c4fed746e858bdde4e
BrettMcAdams96@gmail.com <brettmcadams96@gmail.com>
2020-09-04 13:42:33 -0400

    finished sprint

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,9 +1,37 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
+    render(<CheckoutForm />)
 
-test("form shows success message on submit with form details", () => {});
+    const header = screen.getByText(/checkout form/i)
+
+    expect(header).toBeInTheDocument();
+});
+
+test("form shows success message on submit with form details", () => {
+    render(<CheckoutForm />)
+
+    const fnInput = screen.getByLabelText(/first name:/i)
+    const lnInput = screen.getByLabelText(/last name:/i)
+    const addressInput = screen.getByLabelText(/address:/i)
+    const cityInput = screen.getByLabelText(/city:/i)
+    const stateInput = screen.getByLabelText(/state:/i)
+    const zipInput = screen.getByLabelText(/zip:/i)
+
+    fireEvent.change(fnInput, { target: { value: 'Brett' } });
+    fireEvent.change(lnInput, { target: { value: 'McAdams' } });
+    fireEvent.change(addressInput, { target: { value: '222 Coding Dr' } });
+    fireEvent.change(cityInput, { target: { value: 'Orlando' } });
+    fireEvent.change(stateInput, { target: { value: 'Fl' } });
+    fireEvent.change(zipInput, { target: { value: '32804' } });
+
+    const checkoutButton = screen.getByText(/checkout here/i);
+    fireEvent.click(checkoutButton);
+
+    const plantOrder = screen.getByText(/brett/i);
+    expect(plantOrder).toBeInTheDocument();
+});
--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,1 +1,15 @@
 // write your custom hook here to control your checkout form
+import { useLocalstorage } from './useLocalStorage';
+
+const useForm = (key, initialValue) => {
+
+    const [values, setValues] = useLocalstorage(key, initialValue);
+
+    const handleChanges = (e) => {
+        setValues({ ...values, [e.target.name]: e.target.value });
+    };
+
+    return [values, handleChanges];
+};
+
+export default useForm
